2b0e454077e626b29d8a8bcc247c4240a81b2cb0
Safa <GallaliSafa@gmail.com>
2023-12-07 13:33:02 +0100

    ajout panier

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -51,7 +51,7 @@ describe('template spec', () => {
     cy.get('input[name="email"]').type( 'sousou@gmail.com')
     cy.get('input[name="password"]').type( '987654321')
     cy.get('button[type="submit"]').click()
-    cy.contains('Invalid email or password')
+    cy.contains('Invalid email or password').should('exist')
 
   })
   it('should be not connect if password is wrong', () => { 
@@ -60,7 +60,42 @@ describe('template spec', () => {
     cy.get('input[name="email"]').type( 'Gallali@test.fr')
     cy.get('input[name="password"]').type( '123456')
     cy.get('button[type="submit"]').click()
-    cy.contains('Invalid email or password')
+    cy.contains('Invalid email or password').should('exist')
 
   })
+
+  it('should have a product in cart', () => {
+    cy.visit(baseUrl)
+   //nous sommes rediriges sur la page cart
+   
+   cy.get('.header a[href="/cart"]').click()
+   cy.visit(baseUrl + '/cart')
+  
+ })
+ it('show a product in cart', () => {
+  //rediriger vers la page cart
+  cy.visit(baseUrl)
+  //regiriger vers la page women
+  cy.get('.header a[href="/women"]').click()
+  //cy.visit(baseUrl + '/women')
+  //rediriger vers le lien de produit selectionne
+  cy.contains('.listing-tem', 'Alphaboost shoes').click()
+  //cy.get('form').find('.listing-tem img').should('contains','My-Logoalt="Alphaboost shoes').click()
+  cy.visit(baseUrl + '/women/alphaboost-shoes-20')
+  //add to cart
+  cy.intercept('https://demo.evershop.io/women/alphaboost-shoes-20*').as('selectOption')
+
+  cy.get('.variant-option-list a').contains('L').click()
+  cy.wait('@selectOption')
+  cy.get('.variant-option-list a').contains('Blue').click()
+  cy.wait('@selectOption')
+  cy.contains('.mt-1',  'ADD TO CART').click()
+  //cy.intercept('https://demo.evershop.io/api/cart/mine/items*').as('addTocart')
+
+  //cy.get('.popup-thumbnail flex justify-center a').contains('VIEW CART (1)').click()
+  //cy.wait('@addTocart')
+
+  cy.contains('.toast-mini-cart a',  'VIEW CART (1)').click()
+  cy.contains('.name','Alphaboost shoes').should('exist')
+})
 })
